f2235b5d2ccecd5eb605362a961866a148f4d772
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-07-31 15:24:14 -0500

    Verificar creador

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const proyectoController = require('../controllers/proyectoController');
+const auth = require('../middleware/auth');
 
 // Crea un proyecto
 // api/proyectos
 router.post('/',
-
+    auth,
     proyectoController.crearProyecto
-)
+);
+
+router.get('/',
+    auth,
+    proyectoController.crearProyecto
+);
 
 module.exports = router;
