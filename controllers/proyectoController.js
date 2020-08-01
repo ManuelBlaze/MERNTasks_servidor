5521181961180b8149babef2fa63f5728c98c8b0
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-07-31 21:34:25 -0500

    Filtrar Proy-Usuario

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -1,7 +1,14 @@
 const Proyecto = require('../models/Proyecto');
+const { validationResult } = require('express-validator');
 
 
 exports.crearProyecto = async (req, res) => {
+
+    // Revisar si hay errores
+    const errores = validationResult(req);
+    if ( !errores.isEmpty() ) {
+        return res.status(400).json({ errores: errores.array() });
+    }
 
     try {
         //Crear un nuevo Proyecto
@@ -14,6 +21,17 @@ exports.crearProyecto = async (req, res) => {
         proyecto.save();
         res.json(proyecto);
 
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
+
+//Obtener proyectos del usuario actual
+exports.obtenerProyectos = async (req, res) => {
+    try {
+        const proyectos = await Proyecto.find({ creador: req.usuario.id }).sort({ creado: -1 });
+        res.json({ proyectos });
     } catch (error) {
         console.log(error);
         res.status(500).send('Hubo un error');
