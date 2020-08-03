38bad746c2c5c3cae3ef51bb85b07de8581da73b
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-08-02 21:32:40 -0500

    Actualizar proyecto

--- routes/proyectos.js
+++ routes/proyectos.js
@@ -14,9 +14,19 @@ router.post('/',
     proyectoController.crearProyecto
 );
 
+//Obtener todos los proyectos
 router.get('/',
     auth,
     proyectoController.obtenerProyectos
 );
 
+//Actualizar Proyecto via ID
+router.put(
+	"/:id",
+	auth,
+	[
+        check("nombre", "El nombre del proyecto es obligatoro").not().isEmpty()
+    ],
+	proyectoController.actualizarProyecto
+);
 module.exports = router;
