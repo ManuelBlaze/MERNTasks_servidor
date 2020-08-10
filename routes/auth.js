5297a150577fc15cda0b407de8c9b2c78b1bc01b
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-08-09 21:48:53 -0500

    routhes

--- routes/auth.js
+++ routes/auth.js
@@ -9,12 +9,12 @@ const auth = require("../middleware/auth");
 //api/auth
 router.post(
 	"/",
-	[
-		check("email", "Agrega un email valido").isEmail(),
-		check("password", "El password debe ser minimo de 6 caracteres").isLength({
-			min: 6,
-		}),
-    ],
+	// [
+	// 	check("email", "Agrega un email valido").isEmail(),
+	// 	check("password", "El password debe ser minimo de 6 caracteres").isLength({
+	// 		min: 6,
+	// 	}),
+    // ],
     authController.autenticarUsuario
 );
 
