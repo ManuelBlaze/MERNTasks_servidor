39932780e035979d431bd115049f94eedd418613
ManuelBlaze <manuel.escobar1217@gmail.com>
2020-08-09 19:18:15 -0500

    proc pass

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,7 +51,7 @@ exports.autenticarUsuario = async (req, res) => {
 //Obtiene el usuario autenticado
 exports.usuarioAutenticado = async ( req, res ) => {
     try {
-        const usuario = await Usuario.findById(req.usuario.id);
+        const usuario = await Usuario.findById(req.usuario.id).select('-password');
         res.json({usuario});
     } catch (error) {
         console.log(error);
